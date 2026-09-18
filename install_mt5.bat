d4ca4c8e826e15a6bf4795e034a2d8e486910266
111abraham <111abraham@users.noreply.github.com>
2026-09-18 09:34:04 +0300

    chore: add install_mt5.bat launcher

---
+++ install_mt5.bat
@@ -0,0 +1,5 @@
+@echo off
+echo ========================================================
+echo   Launching MetaTrader 5 Setup for FundedNext Bridge
+echo ========================================================
+start "" "%~dp0installers\mt5setup.exe"
